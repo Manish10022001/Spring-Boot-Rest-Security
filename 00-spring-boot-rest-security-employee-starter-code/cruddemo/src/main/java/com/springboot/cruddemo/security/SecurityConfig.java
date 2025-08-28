1f2d6cb4c9ca228802839eba855b09f86721eba4
Manish10022001 <manishshirsat2850@gmail.com>
2025-08-28 19:20:34 +0530

    restrict access based on roles

--- 00-spring-boot-rest-security-employee-starter-code/cruddemo/src/main/java/com/springboot/cruddemo/security/SecurityConfig.java
+++ 00-spring-boot-rest-security-employee-starter-code/cruddemo/src/main/java/com/springboot/cruddemo/security/SecurityConfig.java
@@ -2,9 +2,13 @@ package com.springboot.cruddemo.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
 
 //step 1
 @Configuration
@@ -33,4 +37,26 @@ public class SecurityConfig {
     //since we defined users here, spring boot will not use user/pass from application.properties file
         return new InMemoryUserDetailsManager(leslie, emma, jaun);
     }
+    //4
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
+        http.authorizeHttpRequests(configurer ->
+                configurer
+                        .requestMatchers(HttpMethod.GET,"/employees").hasRole("EMPLOYEE")
+                        .requestMatchers(HttpMethod.GET, "/employees/**").hasRole("EMPLOYEE") // USED **, means all sub-paths
+                        .requestMatchers(HttpMethod.POST,"/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.PUT,"/employees").hasRole("MANAGER")
+                        .requestMatchers(HttpMethod.DELETE,"/employees/**").hasRole("ADMIN")
+        );
+
+        //use HTTP Basic authentication
+        http.httpBasic(Customizer.withDefaults());
+
+        //disable Cross Site Request Forgery(CSRF)
+        //in general, not required for stateless REST APIs that use POST, PUT, DELETE, and/or PATCH
+        http.csrf(csrf->csrf.disable());
+
+        return http.build();
+    }
+
 }
